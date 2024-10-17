3053c735d43f013ce685dbef078cec8621a39772
SenecaX <kedaren.dorasami@gmail.com>
2024-10-17 10:56:14 +0400

    updates

--- apps/backend-v1/src/types/authenticatedRequest.ts
+++ apps/backend-v1/src/types/authenticatedRequest.ts
@@ -1,6 +1,10 @@
 import { Request } from 'express';
 import { IUser } from '../models/user.model';
 
-export interface AuthenticatedRequest extends Request {
+// Add generics for params, query, and body
+export interface AuthenticatedRequest<P = any, ResBody = any, ReqBody = any, ReqQuery = any> extends Request {
   user: IUser;
+  params: P;      // Generic type for route params
+  body: ReqBody;  // Generic type for request body
+  query: ReqQuery; // Generic type for query params
 }
